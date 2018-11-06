43c4affa16ee47fe52e062507a0f9f368360136e
pinchukovartur <pinchukovartur@gmail.com>
2018-11-06 14:49:27 +0300

    add dev and prod servers

--- bonusmanager/constant.py
+++ bonusmanager/constant.py
@@ -1,2 +1,4 @@
-server_url = "https://treasures-development.appspot.com/"
+server_dev_url = "https://treasures-development.appspot.com/"
+server_prod_url = "https://treasures-production.appspot.com/"
 local_server_url = "http://192.168.88.29:8080/"
+server_key = "6b7b1a88b2aa45eb9f861d9c86e67696"
--- bonusmanager/views.py
+++ bonusmanager/views.py
@@ -2,7 +2,7 @@ import json
 import time
 from django.shortcuts import render, redirect
 
-from .constant import server_url
+from .constant import local_server_url, server_prod_url, server_dev_url, server_key
 
 import requests
 
@@ -15,6 +15,12 @@ def index(request):
 
 
 def find_game_state_bonuses(request):
+    """
+    find  player bonus state by PlayerID on the server
+    if game state is not find, that we are not added bonuses for this player
+    :param request:
+    :return:
+    """
     if not request.user.is_authenticated:
         return redirect('/admin')
 
@@ -27,8 +33,20 @@ def find_game_state_bonuses(request):
     if not request.POST["game_state_id"]:
         return render(request, 'bonusmanager/index.html', {"error": "game state id can't be null!"})
 
+    if not "server" in request.POST.keys():
+        return render(request, 'bonusmanager/index.html', {"error": "server param can't be null!"})
+
+    if not request.POST["server"]:
+        return render(request, 'bonusmanager/index.html', {"error": "server can't be null!"})
+
+    server_url = local_server_url
+    if request.POST["server"] == "prod":
+        server_url = server_prod_url
+    elif request.POST["server"] == "dev":
+        server_url = server_dev_url
+
     game_state_id = request.POST["game_state_id"]
-    r = requests.get(server_url + "get_bonus_resource?game_state_id=" + game_state_id + "&all_bonuses=true")
+    r = requests.get(server_url + "get_bonus_resource?game_state_id=" + game_state_id + "&all_bonuses=true&key=" + server_key)
 
     print(r.text)
     response = json.loads(r.text)
@@ -40,10 +58,16 @@ def find_game_state_bonuses(request):
         return render(request, 'bonusmanager/bonus_table.html', {"game_state_id": game_state_id})
 
     return render(request, 'bonusmanager/bonus_table.html', {"resources": response["resources"],
-                                                             "game_state_id": game_state_id})
+                                                             "game_state_id": game_state_id,
+                                                             "server": request.POST["server"]})
 
 
 def add_bonus(request):
+    """
+    Create state and add bonuses for player by  ID
+    :param request:
+    :return:
+    """
     if not request.user.is_authenticated:
         return redirect('/admin')
 
@@ -56,17 +80,34 @@ def add_bonus(request):
     if not request.POST["game_state_id"] or not request.POST["count"] or not request.POST["type_bonus"]:
         return render(request, 'bonusmanager/index.html', {"error": "param can't be null!"})
 
+    if not "server" in request.POST.keys():
+        return render(request, 'bonusmanager/index.html', {"error": "server param can't be null!"})
+
+    if not request.POST["server"]:
+        return render(request, 'bonusmanager/index.html', {"error": "server can't be null!"})
+
+    server_url = local_server_url
+    if request.POST["server"] == "prod":
+        server_url = server_prod_url
+    elif request.POST["server"] == "dev":
+        server_url = server_dev_url
+
     game_state_id = request.POST["game_state_id"]
     count = request.POST["count"]
     type_bonus = request.POST["type_bonus"]
 
     requests.get(server_url + "save_bonus_resource?game_state_id=" + game_state_id +
-                 "&bonus_resources_type=" + type_bonus + "&bonus_resources_count=" + count)
+                 "&bonus_resources_type=" + type_bonus + "&bonus_resources_count=" + count + "&key=" + server_key)
     time.sleep(1)
     return find_game_state_bonuses(request)
 
 
 def del_bonus(request):
+    """
+    Remove player state  from server
+    :param request:
+    :return:
+    """
     if not request.user.is_authenticated:
         return redirect('/admin')
 
@@ -79,10 +120,22 @@ def del_bonus(request):
     if not request.POST["game_state_id"] or not request.POST["create_at"]:
         return render(request, 'bonusmanager/index.html', {"error": "param can't be null!"})
 
+    if not "server" in request.POST.keys():
+        return render(request, 'bonusmanager/index.html', {"error": "server param can't be null!"})
+
+    if not request.POST["server"]:
+        return render(request, 'bonusmanager/index.html', {"error": "server can't be null!"})
+
+    server_url = local_server_url
+    if request.POST["server"] == "prod":
+        server_url = server_prod_url
+    elif request.POST["server"] == "dev":
+        server_url = server_dev_url
+
     game_state_id = request.POST["game_state_id"]
     create_at = request.POST["create_at"]
 
     requests.get(server_url + "del_bonus_resource?game_state_id=" + game_state_id +
-                 "&create_at_bonus=" + create_at)
+                 "&create_at_bonus=" + create_at+ "&key=" + server_key)
     time.sleep(1)
     return find_game_state_bonuses(request)
